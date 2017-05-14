edf6d45c1e56003efdf13d35c0896a4ca51202c8
rahuldcoder <rahulthakur02k@gmail.com>
2017-05-15 05:11:19 +0530

    starting of our blog

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Index Page</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="index_style.css">
+</head>
+<body>
+<!--Our top menu bar -->
+<div class="topnav">
+	<a class="active" href="#home">Home</a>
+	<a href="#news">News</a>
+	<a href="#contact">Contact-Us</a>
+	<a href="#about">About-Us</a>
+</div>
+
+<div class="hero-image">
+	<div class="hero-text">
+		<h1> Dragon Ball Super </h1>
+		<p> Unleash The Saiyan Power </p>
+		<button> Facts </button>
+	</div>	
+</div>
+</body>
+</html>
